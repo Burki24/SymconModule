40df97227b2fe557fcd8a6e16f90cb802e5167b0
Burki24 <burkhard@kneiseler.de>
2021-11-07 10:38:35 +0100

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:Burki24/SymconModule.git master:gh-pages
 
-cd docs/.vuepress
+cd ./docs/.vuepress
 remove ./dist
 cd -
